595b1be6912d2ea95cfcf5752ffcd66aae79c3a5
jcwdias <jeanwitcel01@gmail.com>
2022-03-01 01:10:57 -0300

    First Commit

---
+++ src/PrimeiroPrograma.java
@@ -0,0 +1,22 @@
+import model.Gato;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+
+		Gato gato1 = new Gato();
+		
+		//gato1.setNome("Juca");;
+		
+		System.out.println(gato1);
+		
+/*	}
+		int a = 1;
+		int b = 4;
+		System.out.println("Hello World!" + (a+b));
+
+	}*/
+	}
+	
+	
+}
